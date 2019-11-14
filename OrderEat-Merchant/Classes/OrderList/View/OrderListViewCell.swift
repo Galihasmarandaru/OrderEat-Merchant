a06c5e0f28dbc4ca970fa1c476e1304a8b35764c
santmalvin17 <malvinsantoso023@gmail.com>
2019-11-14 12:32:22 +0700

    OrderList Details Done

---
+++ OrderEat-Merchant/Classes/OrderList/View/OrderListViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  OrderListViewCell.swift
+//  OrderEat-Merchant
+//
+//  Created by Malvin Santoso on 14/11/19.
+//  Copyright © 2019 Galih Asmarandaru. All rights reserved.
+//
+
+import UIKit
+
+class OrderListViewCell: UITableViewCell {
+    @IBOutlet weak var orderNo: UILabel!
+    @IBOutlet weak var pickUpTime: UILabel!
+    @IBOutlet weak var customerDetails: UILabel!
+    @IBOutlet weak var orderPrice: UILabel!
+    @IBOutlet weak var orderDate: UILabel!
+    @IBOutlet weak var orderStatus: UILabel!
+    
+}
---
+++ OrderListViewController.swift
@@ -0,0 +1,50 @@
+//
+//  OrderListViewController.swift
+//  OrderEat-Merchant
+//
+//  Created by Malvin Santoso on 14/11/19.
+//  Copyright © 2019 Galih Asmarandaru. All rights reserved.
+//
+
+import UIKit
+
+class OrderListViewController: UIViewController {
+
+    @IBOutlet weak var detailsTableView: UITableView!
+    
+    var color : UIColor!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add", style: .plain, target: self, action: nil)
+        // Do any additional setup after loading the view.
+        
+        detailsTableView.backgroundColor = color
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension OrderListViewController: UITableViewDelegate,UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! OrderListViewCell
+        return cell
+    }
+    
+    
+}
